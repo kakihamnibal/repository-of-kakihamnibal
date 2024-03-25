17df80ae7d669190b8d556b843629d301112e8f4
kakihamnibal <pocchama132634@outlook.jp>
2024-03-26 02:54:08 +0900

    削除の実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,12 +12,26 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'> <a href="/posts/{{ $post->id }}">{{ $post->title }}</h2>
+                    <h2 class='title'> <a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
                 </div>
+                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                </form>
             @endforeach
         </div>
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
+        <script>
+            function deletePost(id) {
+                'use strict'
+
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
